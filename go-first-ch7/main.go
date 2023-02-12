28e72b2d92cf8756520fa8778ce17b401e401a7d
luohuidong <luohuidong01@126.com>
2023-02-12 22:08:19 +0800

    first commit

---
+++ go-first-ch7/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	_ "github.com/go-redis/redis/v8" // “_”为空导入
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	logrus.Println("hello, go module mode")
+	logrus.Println(uuid.NewString())
+}
